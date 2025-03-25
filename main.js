a69ca72dcff6ab767477b32baf29ba9a1f4b3b04
kabir-coderex <kabir@coderex.co>
2025-03-25 10:17:09 +0600

    update icon and behaibiour

--- main.js
+++ main.js
@@ -1,39 +1,82 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, Tray, Menu, globalShortcut } = require('electron');
 const path = require('path');
 
 let mainWindow;
+let tray;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 600,
+    show: false, // Start hidden
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
     icon: path.join(__dirname, 'assets/icons/copy_list.icns'),
+    frame: false, // Remove window frame
+    resizable: false,
   });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+}
+
+app.whenReady().then(() => {
+  // Hide from the Dock
+  if (app.dock) app.dock.hide();
+
+  // Enable auto-launch at login
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    openAsHidden: true,
+    path: app.getPath('exe'),
+  });
+
+  // Create the Tray icon
+  tray = new Tray(path.join(__dirname, 'assets/icons/copy_list.png')); // Use a transparent PNG for macOS
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', click: () => mainWindow.show() },
+    { label: 'Hide', click: () => mainWindow.hide() },
+    { type: 'separator' },
+    { label: 'Quit', click: () => app.quit() }
+  ]);
+
+  tray.setToolTip('Copy List App');
+
+  // **Handle Left Click (Show Window)**
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+      mainWindow.focus();
+    }
+  });
+
+  // **Handle Right Click (Show Menu)**
+  tray.on('right-click', () => {
+    tray.popUpContextMenu(contextMenu);
+  });
+
+  createWindow();
 
   // Register hotkey (Ctrl + Shift + V)
   globalShortcut.register('Control+Shift+V', () => {
     if (mainWindow.isVisible()) {
-      mainWindow.show();  // Show the window if it's hidden
+      mainWindow.hide();
     } else {
-      mainWindow.show();  // Show the window if it's not open
+      mainWindow.show();
+      mainWindow.focus();
     }
   });
-}
+});
 
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on('window-all-closed', (e) => {
+  e.preventDefault(); // Prevent quitting when window is closed
 });
 
 app.on('will-quit', () => {
-  // Unregister the hotkey when the app quits
-  globalShortcut.unregisterAll();
+  globalShortcut.unregisterAll(); // Unregister shortcuts
 });
